cd09be06aed470516da55ec822ce67c636fb0473
TMB <Troy.Boettger@gmail.com>
2021-04-12 13:09:19 -0400

    begin setup of api

---
+++ backend/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"./database"
+	"./routes"
+	"github.com/gofiber/fiber"
+)
+
+func main() {
+	database.Connect()
+
+	app := fiber.New()
+	routes.Setup(app)
+
+	app.Listen(":8000")
+}
