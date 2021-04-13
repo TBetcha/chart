f5fcb79b2ddcb629d4701ba2bb75057794b9e634
TMB <Troy.Boettger@gmail.com>
2021-04-12 22:27:09 -0400

    crud user

--- backend/controllers/authController.go
+++ backend/controllers/authController.go
@@ -5,7 +5,6 @@ import (
 	"../models"
 	"../util"
 	"github.com/gofiber/fiber"
-	"golang.org/x/crypto/bcrypt"
 	"strconv"
 	"time"
 )
@@ -24,14 +23,12 @@ func Register(c *fiber.Ctx) error {
 		})
 	}
 
-	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
-
 	user := models.User{
 		FirstName: data["first_name"],
 		LastName:  data["last_name"],
 		Email:     data["email"],
-		Password:  password,
 	}
+	user.SetPassword(data["password"])
 	database.DB.Create(&user)
 
 	return c.JSON(user)
@@ -52,7 +49,7 @@ func Login(c *fiber.Ctx) error {
 			"message": "user not found",
 		})
 	}
-	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
+	if err := user.ComparePassword(data["password"]); err != nil {
 		c.Status(400)
 		return c.JSON(fiber.Map{
 			"message": "incorrect password",
