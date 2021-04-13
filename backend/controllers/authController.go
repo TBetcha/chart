83083f61aaffa55d5a29ca8cf1077857729ce7ba
TMB <Troy.Boettger@gmail.com>
2021-04-12 21:49:52 -0400

    start middleware

--- backend/controllers/authController.go
+++ backend/controllers/authController.go
@@ -3,7 +3,7 @@ package controllers
 import (
 	"../database"
 	"../models"
-	"github.com/dgrijalva/jwt-go"
+	"../util"
 	"github.com/gofiber/fiber"
 	"golang.org/x/crypto/bcrypt"
 	"strconv"
@@ -59,17 +59,47 @@ func Login(c *fiber.Ctx) error {
 		})
 	}
 
-	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
-		Issuer: strconv.Itoa(int(user.Id)),
-		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(), // 1 day
-	})
-	token,err := claims.SignedString([]byte("secret"))
+	token, err := util.GenerateJwt(strconv.Itoa(int(user.Id)))
 
-	if err != nil{
+	if err != nil {
 		return c.SendStatus(fiber.StatusInternalServerError)
 	}
 
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    token,
+		Expires:  time.Now().Add(time.Hour * 24),
+		HTTPOnly: true,
+	}
+	c.Cookie(&cookie)
 
-	return c.JSON(token)
+	return c.JSON(fiber.Map{
+		"message": "Success",
+	})
+}
+
+func User(c *fiber.Ctx) error {
+	cookie := c.Cookies("jwt")
+
+	id, _ := util.ParseJwt(cookie)
+
+	var user models.User
+
+	database.DB.Where("id = ?", id).First(&user)
+
+	return c.JSON(user)
+}
+
+func Logout(c *fiber.Ctx) error {
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    "",
+		Expires:  time.Now().Add(-time.Hour * 24),
+		HTTPOnly: true,
+	}
+	c.Cookie(&cookie)
+	return c.JSON(fiber.Map{
+		"message": "Logout",
+	})
 
 }
--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -2,10 +2,18 @@ package routes
 
 import (
 	"../controllers"
+	"../middleware"
 	"github.com/gofiber/fiber"
 )
 
 func Setup(app *fiber.App) {
 	app.Post("/api/register", controllers.Register)
 	app.Post("/api/login", controllers.Login)
+
+	//Private routes - use middleware
+
+	app.Use(middleware.IsAuthenticated)
+
+	app.Get("/api/user", controllers.User)
+	app.Post("api/logout", controllers.Logout)
 }
