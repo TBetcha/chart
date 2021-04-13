dbb5cdc7d1d1ee8f3825d2bd59e47a80d2c81414
TMB <Troy.Boettger@gmail.com>
2021-04-12 20:53:19 -0400

    get jwt

--- backend/controllers/authController.go
+++ backend/controllers/authController.go
@@ -3,8 +3,11 @@ package controllers
 import (
 	"../database"
 	"../models"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber"
 	"golang.org/x/crypto/bcrypt"
+	"strconv"
+	"time"
 )
 
 func Register(c *fiber.Ctx) error {
@@ -55,5 +58,18 @@ func Login(c *fiber.Ctx) error {
 			"message": "incorrect password",
 		})
 	}
-	return c.JSON(user)
+
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Issuer: strconv.Itoa(int(user.Id)),
+		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(), // 1 day
+	})
+	token,err := claims.SignedString([]byte("secret"))
+
+	if err != nil{
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+
+	return c.JSON(token)
+
 }
---
+++ backend/database/connect.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"../models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+
+
+//	db, err := gorm.Open(mysql.Open(os.Getenv("USER")+":"+os.Getenv("PASSWORD")+"@/"+os.Getenv("DBNAME")), &gorm.Config{})
+	db,err := gorm.Open(mysql.Open("root:Rollout3@/mars"), &gorm.Config{})
+
+	if err != nil {
+		panic("Could not connect to the DB")
+	}
+
+	DB = db
+
+	db.AutoMigrate(&models.User{})
+	log.Println(db)
+}
